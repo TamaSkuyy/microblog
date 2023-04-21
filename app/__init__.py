ef03b9d60414d225b93bb48c9fb83c1dcc794323
TamaSkuyy <mkhatama@gmail.com>
2023-04-21 19:25:02 +0700

    datetime using flask-moment

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,10 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap5
+from flask_moment import Moment
+from logging.handlers import SMTPHandler, RotatingFileHandler
 import logging
 import os
 
@@ -17,6 +18,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap5(app)
+moment = Moment(app)
 
 from app import routes, models, errors
 
