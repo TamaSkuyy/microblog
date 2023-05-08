b22cf08dbe6f697054f2052aad93185fd817fa86
TamaSkuyy <mkhatama@gmail.com>
2023-05-08 15:27:54 +0700

    Chapter 16: Full-Text Search

--- tests.py
+++ tests.py
@@ -10,6 +10,7 @@ from config import Config
 class TestConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = 'sqlite://'
+    ELASTICSEARCH_URL = None
 class UserModelCase(unittest.TestCase):
     def setUp(self):
         self.app = create_app(TestConfig)
